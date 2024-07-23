64e6849c45aeb05a6dc155378fb5049292f90901
root <ahmed.errajaai@gmail.com>
2024-07-22 17:06:37 -0700

    save

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 /**
- *main prints the name of the file it was compiled from
- * 
- * Return: nth to be retutned
- * */
+*main - prints the name of
+*the file it was compiled from
+*
+*Return: nothing to be returned
+*/
 
-int main()
+int main(void)
 {
 printf("%s\n", __FILE__);
 return (0);
